6638a01e1d869f9620fe19ac7b5d221703c6b024
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-01 11:26:41 -0300

    class2

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // O backend consiga interpretar o formato JSON
+
 app.get("/", (req, res) => {
     return res.send('Hello world!');
+})
+
+app.post("/", (req, res) => {
+    //req.headers - configurações e chaves de autorização
+    //req.body - conjunto de dados que está sendo fornecido como corpo da requisição
+    //req.params - parâmetros estáticos de rota
+    //req.query - query params
+    console.log(req.headers);
+    console.log(req.body);
+    return res.status(201).json({ message: 'Created successfully.'});
 })
 
 app.listen(3000, () => {
